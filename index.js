876aaf5165174b7755d869e24d009363396e6688
Cristina SG <cristinasanchezg@gmail.com>
2022-11-25 10:15:56 -0600

    Iniciar servidor

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const exprexx = require('express');
+const express = require('express');
 const cors = require('cors');
 
 const server = express();
+
 server.use(cors());
+server.use(express.json());
+server.listen(3000, () => {
+    console.log("Servidor iniciado en el puerto 3000");
+});
