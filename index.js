85de2a04410b14812211065752ec7de9bd8cad34
Cristina SG <cristinasanchezg@gmail.com>
2022-11-25 12:54:11 -0600

    Base d datos d cervezas c estilo, presen y precio

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const server = express();
 
 server.use(cors());
 server.use(express.json());
-server.listen(3000, () => {
+
+const PORT = 3000;
+
+let cervezas = [
+    {estilo: 'Blonde', presentacion: 'botella', precio: 55},
+    {estilo: 'Porter', presentacion: 'botella', precio: 60},
+    {estilo: 'Mole porter', presentacion: 'botella', precio: 90},
+];
+
+server.get("/", (request, response) => {
+    response.send("API V1.0");
+});
+server.listen(PORT, () => {
     console.log("Servidor iniciado en el puerto 3000");
 });
