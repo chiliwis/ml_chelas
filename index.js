d7e099c1f8d04d6b4d145352f12fbe87421686f5
Cristina SG <cristinasanchezg@gmail.com>
2022-11-25 18:05:24 -0600

    endpoints

--- index.js
+++ index.js
@@ -9,14 +9,65 @@ server.use(express.json());
 const PORT = 3000;
 
 let cervezas = [
-    {estilo: 'Blonde', presentacion: 'botella', precio: 55},
-    {estilo: 'Porter', presentacion: 'botella', precio: 60},
-    {estilo: 'Mole porter', presentacion: 'botella', precio: 90},
+    { estilo: 'Blonde', presentacion: 'botella', precio: 55 },
+    { estilo: 'Porter', presentacion: 'botella', precio: 60 },
+    { estilo: 'Mole porter', presentacion: 'botella', precio: 90 },
 ];
 
 server.get("/", (request, response) => {
     response.send("API V1.0");
 });
+
+server.get("/cervezas", (request, response) => {
+    let data_mapeada = cervezas.map((cerveza, index) => {
+        return { index, ...cerveza };
+    });
+    response.json(
+        {
+            data: data_mapeada,
+            count: cervezas.length,
+            mensaje: "Cervezas base"
+        }
+    );
+});
+
+server.post("/cervezas", (request, response) => {
+    const cerveza = request.body;
+    if (!cerveza.nombre) {
+        return response.status(400).json({ mensaje: "La cerveza debe tener un estilo" });
+    }
+    cervezas.push(cerveza);
+    response.json(
+        {
+            data: cerveza,
+            mensaje: "Entro a la funcion agregar cervezas"
+        }
+    );
+});
+
+server.put("/cervezas/:index", (request, response) => {
+    const { index } = request.params;
+    const cerveza = request.body;
+    cervezas[index] = cerveza;
+    response.json(
+        {
+            data: index,
+            mensaje: "Se actualizo la cerveza"
+        }
+    );
+});
+
+server.delete("/cervezas/:index", (request, response) => {
+    const { index } = request.params;
+    cervezas.splice(index, 1);
+    response.json(
+        {
+            data: index,
+            mensaje: "Se elimino la cerveza"
+        }
+    );
+});
+
 server.listen(PORT, () => {
     console.log("Servidor iniciado en el puerto 3000");
 });
